2c51bc75a68f3b003b15cccbc81d157fc142c27f
Luckyboyyy9 <muhthoriq0111@gmail.com>
2025-02-20 10:55:00 +0800

    new update

--- database/migrations/2024_12_27_190248_create_mahasiswa_table.php
+++ database/migrations/2024_12_27_190248_create_mahasiswa_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('mahasiswa', function (Blueprint $table) {
             $table->id();
-            $table->string('nim')->unique();
+            $table->bigIncrements('nim'); // Menggunakan auto-increment
             $table->string('nama');
             $table->string('jurusan');
             $table->integer('angkatan');
